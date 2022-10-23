aa8ff9436ef57892a354cb937b7f84b93170c814
Valera Lavashov <Valera Lavashov@DESKTOP-65PH8EI>
2022-10-23 17:37:00 +0300

    Add project files.

---
+++ additional_ex/Address.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Создать класс с именем Address. 
+//В теле класса требуется создать поля: index, country, city, street, house, apartment.Для каждого поля, создать свойство с двумя методами доступа.  
+//Создать экземпляр класса Address.  
+//В поля экземпляра записать информацию о почтовом адресе.  
+//Выведите на экран значения полей, описывающих адрес. 
+namespace additional_ex
+{
+    /// <summary>
+    /// Class Address
+    /// have contains fields: index, country, city, street, house, apartment
+    /// each field must have properities with two permission methods
+    /// </summary>
+    internal class Address
+    {
+        private int index, house, apartment;
+        private string country, city, street;
+        //properities for fields
+        public int Index
+        {
+            
+             get
+            { return index; }   //mutator
+            set
+            {              
+                index = value; //accessor
+            }  
+        }
+        public string Country
+        {
+            private get { return country; }//mutator
+            set { country = value; }    //accessor
+        }
+        public string City
+        {
+            private get { return city; }//mutator
+            set { city = value; }   //accessor
+        }
+        public string Street
+        {
+            private get { return street; } //mutator
+            set { street = value; } //accessor
+
+
+        }
+        public int House
+        {
+            private get { return house; }//mutator
+            set { house = value; }//accessor
+        }
+        public int Apartment
+        {
+            
+            private get //mutator
+            {                
+                return apartment;
+            }
+            set { apartment = value; }//accessor
+        }
+
+        public void Show() //shows values
+        {
+            Console.WriteLine("Index: {0}", this.Index);
+            Console.WriteLine("Country: {0} ", this.Country);
+            Console.WriteLine("City: {0}", this.Index);
+            Console.WriteLine("Street: {0}", this.Street);
+            Console.WriteLine("House: {0}", this.House);
+            Console.WriteLine("Apartments: {0}", this.Apartment);
+        }
+
+        public Address() //constructor
+        {
+            this.Index = index;
+            this.Country = country;
+            this.City = city;
+            this.Street = street;
+            this.House = house;
+            this.Apartment = apartment;
+        }
+
+    }
+}
---
+++ additional_ex/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Создать класс с именем Address. 
+//В теле класса требуется создать поля: index, country, city, street, house, apartment.Для каждого поля, создать свойство с двумя методами доступа.  
+//Создать экземпляр класса Address.  
+//В поля экземпляра записать информацию о почтовом адресе.  
+//Выведите на экран значения полей, описывающих адрес. 
+
+namespace additional_ex
+{
+    /// <summary>
+    /// Main class
+    /// program have to fill address from user
+    /// Additional class: Address
+    /// Fields: index, country, city, street, house, apartment
+    /// </summary>
+    internal class Program
+    {
+        static Address address = new Address(); // creating exemplar
+        static void Main(string[] args)
+        {
+            ValuesFromKeyBoard(); //call method for Address class
+            Console.ReadKey(); //delay
+        }
+
+        public static void ValuesFromKeyBoard() //get values from keyboard
+        {
+            Console.Write("Введите номер индекса: ");
+            string index = Console.ReadLine(); //filling index value
+            ParsingValue(index); //method for parsing value
+            Console.Write("Введите название страны: ");
+            string country = Console.ReadLine(); //filling country value
+            ParsingValue(country); //method for parsing value
+            Console.Write("Введите название города: ");
+            string city = Console.ReadLine(); //filling city value
+            Console.Write("Введите название улицы: ");
+            string street = Console.ReadLine(); //filling steet value
+            Console.Write("Введите номер дома: ");
+            string house = Console.ReadLine(); //filling house value
+            Console.Write("Введите номер квартиры: ");
+            string apartment = Console.ReadLine(); //filling house value
+            address.Show(); //shows all address values
+        }
+        public static void ParsingValue(string value)
+        {
+            int x;
+            if (Int32.TryParse(value, out x))
+            {
+                
+            }
+            else
+            {
+
+            }
+
+        }
+        public static void ClearLine()
+        {
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+        }
+    }
+}
